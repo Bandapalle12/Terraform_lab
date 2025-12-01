ae67ba33e88d1d98c05efcdb683eaaad07f3459f
Bandapalle12 <sourabhbandapalle83@gmail.com>
2025-12-01 23:39:01 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     bucket         = "terraformbackendstore11"      # change to your bucket name
     key            = "ec2-docker-lab/terraform.tfstate"
-    region         = "ap-south-1"
+    region         = "us-east-1"
     dynamodb_table = "terraform-locks"             # change to your DynamoDB table
     encrypt        = true
   }
